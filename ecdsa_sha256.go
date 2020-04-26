af11d551bb490df184cf3ac10d8c9379168cc109
kura <mkurahay@gmail.com>
2020-04-27 00:36:57 +0900

    added ECDSA signging and verification sample codes.

---
+++ ecdsa_sha256.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+)
+
+func main() {
+
+	// P256, P384 or P512
+	ellipticCurve := elliptic.P256()
+
+	//privateKey := new(ecdsa.PrivateKey)
+	privateKey, err := ecdsa.GenerateKey(ellipticCurve, rand.Reader)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//var publicKey ecdsa.PublicKey
+	publicKey := privateKey.PublicKey
+
+	fmt.Printf("private key: %x\n", privateKey)
+	fmt.Printf("public key : %x\n", publicKey)
+
+	//var hash hash.Hash
+	hash := sha256.New()
+	r := big.NewInt(0)
+	s := big.NewInt(0)
+
+	data := "This is data."
+
+	hash.Write(([]byte)(data))
+	hashed := hash.Sum(nil)
+
+	r, s, serr := ecdsa.Sign(rand.Reader, privateKey, hashed)
+	if serr != nil {
+		fmt.Println(serr)
+		return
+	}
+
+	fmt.Printf("r: %x\n", r)
+	fmt.Printf("s: %x\n", s)
+
+	signature := r.Bytes()
+	signature = append(signature, s.Bytes()...)
+
+	fmt.Printf("signature: %x\n", signature)
+
+	// SHA256: key size=32, curve bits=256
+	// SHA384: key size=48, curve bits=384
+	// SHA512: key size=66, curve bits=521 (note: not typo)
+	keySize := 32
+	parsedR := big.NewInt(0).SetBytes(signature[:keySize])
+	parsedS := big.NewInt(0).SetBytes(signature[keySize:])
+
+	fmt.Printf("parsed r: %x\n", parsedR)
+	fmt.Printf("parsed s: %x\n", parsedS)
+
+	verifystatus := ecdsa.Verify(&publicKey, hashed, parsedR, parsedS)
+	fmt.Printf("verify: %t\n", verifystatus)
+}
